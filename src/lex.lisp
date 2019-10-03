ea8c12cd45b7b96e203548ca90d65778fa3b15c7
John Jacobsen <jjacobsen@opploans.com>
2019-10-03 11:18:41 -0500

    Better sentences.

--- src/lex.lisp
+++ src/lex.lisp
@@ -17,25 +17,33 @@
 
 (defnth lifeforms `(algae nanobes viruses))
 (defnth adjective `(,(color) ,(color-pair) slimy oozing gaseous microscopic))
-(defnth evolved `(evolved "started reproducing" "begun self-replicating"))
-(defnth place-adj `(dark deep smoky lava-filled wet icy sunny scorching))
+(defnth evolved `("evolved" "started reproducing" "begun self-replicating"))
+(defnth place-adj `(dark deep smoky lava-filled wet icy sunny scorched))
 (defnth place-noun `(canyons pits valleys mountaintops))
 
+(defun alive-a (planet)
+  `(,(adjective) ,(lifeforms) have ,(evolved)
+     in the ,(place-adj) ,(place-noun) of planet ,(concatenate 'string planet "!")))
 
-(defun itsalive ()
-  (format nil "~@(~{~a ~}~)" `(,(adjective) ,(lifeforms) have ,(evolved)
-                                in the ,(place-adj) ,(place-noun) of planet x!)))
+(defun alive-b (planet)
+  `(in the ,(place-adj) ,(place-noun) of planet ,(concatenate 'string planet ",")
+       ,(adjective) ,(lifeforms) have ,(concatenate 'string (evolved) "!")))
+
+(defun itsalive (planet)
+  (format nil "~@(~{~a ~}~)" (if (zerop (random 2))
+                                 (alive-a "x")
+                                 (alive-b "x"))))
 
 (comment
- (loop repeat 10 collect (itsalive))
- ;;=>
- '("Green algae have started reproducing in the deep valleys of planet x! "
-   "Gaseous algae have started reproducing in the lava-filled canyons of planet x! "
-   "Blue algae have evolved in the dark mountaintops of planet x! "
-   "Microscopic nanobes have started reproducing in the sunny mountaintops of planet x! "
-   "Gaseous viruses have begun self-replicating in the smoky canyons of planet x! "
-   "Slimy viruses have started reproducing in the scorching pits of planet x! "
-   "Oozing viruses have begun self-replicating in the dark mountaintops of planet x! "
-   "Yellow algae have evolved in the wet pits of planet x! "
-   "Microscopic viruses have evolved in the sunny pits of planet x! "
-   "Slimy viruses have started reproducing in the lava-filled canyons of planet x! "))
+  (loop repeat 10 collect (itsalive "X"))
+  ;;=>
+  '("In the wet canyons of planet x, grey-green algae have begun self-replicating! "
+    "Microscopic nanobes have started reproducing in the lava-filled canyons of planet x! "
+    "Greyish-white nanobes have begun self-replicating in the scorched pits of planet x! "
+    "Grey-green nanobes have begun self-replicating in the sunny mountaintops of planet x! "
+    "Purplish-blue algae have evolved in the icy mountaintops of planet x! "
+    "In the dark valleys of planet x, gaseous algae have started reproducing! "
+    "Greyish-yellow viruses have begun self-replicating in the sunny canyons of planet x! "
+    "Oozing viruses have started reproducing in the scorched valleys of planet x! "
+    "Greyish-white viruses have started reproducing in the wet valleys of planet x! "
+    "In the sunny canyons of planet x, oozing viruses have begun self-replicating! "))
